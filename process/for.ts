ca82e9cede8f2520a6e8af20ca5778d6510dcfb7
cui_bingbing <cui_bingbing@topsec.com.cn>
2022-12-15 18:46:38 +0800

    feat:for加入diff

--- process/for.ts
+++ process/for.ts
@@ -5,19 +5,24 @@ class MyFor extends HTMLElement {
                 </div>
                 `;
     iterationTemplate = '';
+    cache: Map<string, HTMLElement> = new Map(); //缓存
+    cacheOptions: Array<any> = [];
     options: Array<any> = [];
     // shadow: ShadowRoot;
     static get observedAttributes() {
         return ['template', 'list'];
     }
     attributeChangedCallback(name: string, oldValue: string, newValue: string) {
-        if (newValue === '[object Object]') {
-            return;
-        }
-        let value = eval(newValue);
-        console.log(name, value, Array.isArray(value));
-        if (Array.isArray(value)) {
-            this.setAttribute(name, {});
+        if (name === 'list') {
+            if (newValue === '[object Object]') {
+                return;
+            }
+            console.log(this.children);
+            let value = eval(newValue);
+            console.log(name, value, Array.isArray(value));
+            if (Array.isArray(value)) {
+                this.setAttribute(name, {});
+            }
         }
     }
     // 劫持setAttribute
@@ -34,17 +39,23 @@ class MyFor extends HTMLElement {
         // this.shadow.innerHTML = `${this.template}`;
     }
     connectedCallback() {
+        console.log(this.children);
+
         setTimeout(() => {
             this.iterationTemplate = this.innerHTML;
             this.setIteration(this.options);
         });
     }
-    // 设置可迭代数据
-    setIteration(list) {
-        setTimeout(() => {
-            let t = ``;
-            for (let ctx of list) {
-                t += this.iterationTemplate!.replace(
+    // 对比新旧数据
+    diff(list: any[]) {
+        let newCache = new Map();
+        let newDOMs: Element[] = list.map((ctx) => {
+            let id = JSON.stringify(ctx);
+            if (this.cache.has(id)) {
+                newCache.set(id, this.cache.get(id));
+                return this.cache.get(id)!;
+            } else {
+                let s = this.iterationTemplate!.replace(
                     /{{[^{}]+}}/g,
                     (target: string) => {
                         return (
@@ -54,8 +65,64 @@ class MyFor extends HTMLElement {
                         );
                     }
                 );
+                let dom = document.createElement('div');
+                dom.innerHTML = s;
+                newCache.set(id, dom.children[0]);
+                return dom.children[0];
             }
-            this?.innerHTML = t;
+        });
+        this.cache = newCache;
+        // 对比新旧DOM;
+        let preDOMs = this.children;
+        let i = 0,
+            j = preDOMs.length - 1;
+        let p = 0,
+            q = newDOMs.length - 1;
+        while (i <= j && p <= q) {
+            if (preDOMs[i] === newDOMs[p]) {
+                // 起始 <=> 起始
+                i++;
+                p++;
+            } else if (preDOMs[j] === newDOMs[q]) {
+                // 结尾 <=> 结尾
+                j--;
+                q--;
+            } else if (preDOMs[i] === newDOMs[q]) {
+                // 起始 <=> 结尾
+                preDOMs[i].replaceWith(newDOMs[q]);
+                i++;
+                q--;
+            } else if (preDOMs[j] === newDOMs[p]) {
+                // 结尾 <=> 起始
+                preDOMs[j].replaceWith(newDOMs[p]);
+                j--;
+                p++;
+            } else {
+                preDOMs[i].replaceWith(newDOMs[p]);
+                i++;
+                p++;
+            }
+        }
+        // 当有新增，减少的节点时
+        if (i > j) {
+            if (p <= q) {
+                preDOMs[j].after(...newDOMs.slice(p, q + 1));
+            }
+        }
+        if (p > q) {
+            if (i <= j) {
+                for (; i <= j; i++) {
+                    preDOMs[i].remove();
+                }
+            }
+        }
+    }
+    // 设置可迭代数据
+    //TODO:配置缓存
+    setIteration(list) {
+        setTimeout(() => {
+            this.diff(list);
+            // this?.innerHTML = t;
         });
     }
 }
